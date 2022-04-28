1120fadbc36ae52e490bbea980cbc26c86453478
pranjalinagarkar <57284040+pranjalinagarkar@users.noreply.github.com>
2022-04-28 13:42:55 +0530

    Update streamlit_app.py
    added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,9 @@ import streamlit
 
 streamlit.title('My Parent\'s New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('🐸Breakfast Menu')
 streamlit.text('Boiled Egg')
+streamlit.text('🍀Spinach')
+streamlit.text('Avacado')
+
+streamlit.header('🍇:banana:Make your own smoothie')
