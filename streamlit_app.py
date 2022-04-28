2637e5ea009af93aa482a10e43f4c56d654aecb0
pranjalinagarkar <57284040+pranjalinagarkar@users.noreply.github.com>
2022-04-28 14:05:26 +0530

    Update streamlit_app.py
    adding code for user to pick fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,8 @@ streamlit.text('🥑Avocado')
 
 streamlit.header('🍇:banana:Make your own smoothie:cherries::apple:')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#lets put a pickist so that user can add fruits as per his/her wish.
+streamlit.multiselect("Pick some fruits : ",list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
