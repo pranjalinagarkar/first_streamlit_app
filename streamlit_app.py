850332b45e333aedb6a0a78d0b53514e3bc6e0ee
pranjalinagarkar <57284040+pranjalinagarkar@users.noreply.github.com>
2022-04-28 14:08:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ streamlit.text('🥑Avocado')
 
 streamlit.header('🍇:banana:Make your own smoothie:cherries::apple:')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #lets put a pickist so that user can add fruits as per his/her wish.
 streamlit.multiselect("Pick some fruits : ",list(my_fruit_list.index))
