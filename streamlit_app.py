80d5158201d2db43b5377be7947028a536610f93
pranjalinagarkar <57284040+pranjalinagarkar@users.noreply.github.com>
2022-04-28 13:54:57 +0530

    Update streamlit_app.py
    adding dataframe and pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('My Parent\'s New Healthy Diner')
 
@@ -8,3 +9,5 @@ streamlit.text('🍀Spinach')
 streamlit.text('🥑Avocado')
 
 streamlit.header('🍇:banana:Make your own smoothie:cherries::apple:')
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
