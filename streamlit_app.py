de49a417e3fc2725cbb3213e2d3a0bc012ce9e4f
pranjalinagarkar <57284040+pranjalinagarkar@users.noreply.github.com>
2022-04-28 12:19:14 +0530

    Update streamlit_app.py
    added more code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Parent\'s New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamit.text('Boiled Egg')
