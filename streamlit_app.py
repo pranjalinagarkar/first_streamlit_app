d82dcbe25f2bc9212dc7b6490b33a5878df5b512
pranjalinagarkar <57284040+pranjalinagarkar@users.noreply.github.com>
2022-05-01 13:57:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas as pd
-import snowflake.connector
+
 
 streamlit.title('My Parent\'s New Healthy Diner')
 
@@ -32,4 +32,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-
+import snowflake.connector
